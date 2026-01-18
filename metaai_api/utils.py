d33991dda07e4b21db85a495c0c87ba8a97d6f52
missquental <missquental@gmail.com>
2026-01-18 11:01:35 +0700

    Update utils.py

--- metaai_api/utils.py
+++ metaai_api/utils.py
@@ -1,1 +1,12 @@
-from .metaai import something
+import requests
+import json
+import re
+
+def build_headers():
+    return {
+        "User-Agent": "Mozilla/5.0",
+        "Content-Type": "application/json"
+    }
+
+def extract_video_urls(response_text):
+    return re.findall(r'https://[^"\']+\.mp4', response_text)
